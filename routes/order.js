25e6b526a740989a27765220ec638909ae4265f5
ChínhKhôngBéo <63283198+buingocchinh321@users.noreply.github.com>
2022-06-26 21:00:40 +0700

    CHANGEEE

--- routes/order.js
+++ routes/order.js
@@ -1,3 +1,9 @@
+/**
+ * Created by vinhnt on 6/16/2017.
+ */
+
+
+
 var express = require('express');
 var router = express.Router();
 var $ = require('jquery');
@@ -7,11 +13,12 @@ router.get('/', function(req, res, next){
 });
 
 router.get('/create_payment_url', function (req, res, next) {
+    
     var dateFormat = require('dateformat');
     var date = new Date();
-    var amount = req.query.amount;
-    var desc = 'Thanh toan don hang Van Transport thoi gian: ' + dateFormat(date, 'yyyy-mm-dd HH:mm:ss');
-    res.render('order', {title: 'Hóa đơn Van Transport', amount: ~~amount, description: desc})
+
+    var desc = 'Thanh toan don hang thoi gian: ' + dateFormat(date, 'yyyy-mm-dd HH:mm:ss');
+    res.render('order', {title: 'Tạo mới đơn hàng', amount: 10000, description: desc})
 });
 
 router.post('/create_payment_url', function (req, res, next) {
@@ -44,7 +51,7 @@ router.post('/create_payment_url', function (req, res, next) {
     }
     var currCode = 'VND';
     var vnp_Params = {};
-    vnp_Params['vnp_Version'] = '2';
+    vnp_Params['vnp_Version'] = '2.1.0';
     vnp_Params['vnp_Command'] = 'pay';
     vnp_Params['vnp_TmnCode'] = tmnCode;
     // vnp_Params['vnp_Merchant'] = ''
@@ -64,20 +71,14 @@ router.post('/create_payment_url', function (req, res, next) {
     vnp_Params = sortObject(vnp_Params);
 
     var querystring = require('qs');
-    var signData = secretKey + querystring.stringify(vnp_Params, { encode: false });
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+    vnp_Params['vnp_SecureHash'] = signed;
+    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
 
-    var sha256 = require('sha256');
-
-    var secureHash = sha256(signData);
-
-    vnp_Params['vnp_SecureHashType'] =  'SHA256';
-    vnp_Params['vnp_SecureHash'] = secureHash;
-    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: true });
-
-    //Neu muon dung Redirect thi dong dong ben duoi
-    res.status(200).json({code: '00', data: vnpUrl})
-    //Neu muon dung Redirect thi mo dong ben duoi va dong dong ben tren
-    //res.redirect(vnpUrl)
+    res.redirect(vnpUrl)
 });
 
 router.get('/vnpay_return', function (req, res, next) {
@@ -95,13 +96,12 @@ router.get('/vnpay_return', function (req, res, next) {
     var secretKey = config.get('vnp_HashSecret');
 
     var querystring = require('qs');
-    var signData = secretKey + querystring.stringify(vnp_Params, { encode: false });
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
 
-    var sha256 = require('sha256');
-
-    var checkSum = sha256(signData);
-
-    if(secureHash === checkSum){
+    if(secureHash === signed){
         //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
 
         res.render('success', {code: vnp_Params['vnp_ResponseCode']})
@@ -121,13 +121,13 @@ router.get('/vnpay_ipn', function (req, res, next) {
     var config = require('config');
     var secretKey = config.get('vnp_HashSecret');
     var querystring = require('qs');
-    var signData = secretKey + querystring.stringify(vnp_Params, { encode: false });
-    
-    var sha256 = require('sha256');
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");     
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+     
 
-    var checkSum = sha256(signData);
-
-    if(secureHash === checkSum){
+    if(secureHash === signed){
         var orderId = vnp_Params['vnp_TxnRef'];
         var rspCode = vnp_Params['vnp_ResponseCode'];
         //Kiem tra du lieu co hop le khong, cap nhat trang thai don hang va gui ket qua cho VNPAY theo dinh dang duoi
@@ -138,20 +138,18 @@ router.get('/vnpay_ipn', function (req, res, next) {
     }
 });
 
-function sortObject(o) {
-    var sorted = {},
-        key, a = [];
-
-    for (key in o) {
-        if (o.hasOwnProperty(key)) {
-            a.push(key);
-        }
-    }
-
-    a.sort();
-
-    for (key = 0; key < a.length; key++) {
-        sorted[a[key]] = o[a[key]];
+function sortObject(obj) {
+	var sorted = {};
+	var str = [];
+	var key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
     }
     return sorted;
 }
